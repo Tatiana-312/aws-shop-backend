e49c6dae9dee9044bfadcd2cd8c020144234b4d8
Tatiana Kuznetsova <tatiana_kuznetsova@epam.com>
2023-11-16 18:56:16 +0600

    create test lambda

--- ProductService/lib/product_service-stack.ts
+++ ProductService/lib/product_service-stack.ts
@@ -1,16 +1,24 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as path from "path";
 
-export class ProductServiceStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class ProductServiceStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'ProductServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const getProductsList = new NodejsFunction(this, "getProductsListHandler", {
+      runtime: Runtime.NODEJS_18_X,
+      entry: path.join(__dirname + "/../handlers/getProductsList.ts"),
+      handler: "handler",
+      bundling: {
+        sourceMap: true,
+        minify: true,
+      },
+      environment: {
+        NODE_OPTIONS: "--enable-source-maps",
+      },
+    });
   }
 }
