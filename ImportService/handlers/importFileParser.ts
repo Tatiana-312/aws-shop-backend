f21399111dcfd526411beb8179b6ecfdf4bf6b53
Tatiana Kuznetsova <tatiana_kuznetsova@epam.com>
2023-12-03 19:31:02 +0600

    refactor

--- ImportService/handlers/importFileParser.ts
+++ ImportService/handlers/importFileParser.ts
@@ -1,12 +1,8 @@
 import { S3Event } from "aws-lambda";
 import csv = require("csv-parser");
-import {
-  CopyObjectCommand,
-  DeleteObjectCommand,
-  GetObjectCommand,
-  S3Client,
-} from "@aws-sdk/client-s3";
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { Readable } from "stream";
+import { moveParsedFile } from "../utils/moveParsedFile";
 
 export const handler = async (event: S3Event) => {
   console.log(`
@@ -16,12 +12,12 @@ export const handler = async (event: S3Event) => {
   const result: any = [];
 
   const key = event.Records[0].s3.object.key;
-  const bucket = event.Records[0].s3.bucket.name;
+  const bucketName = event.Records[0].s3.bucket.name;
 
   const s3Client = new S3Client();
 
   const getObjectCommand = new GetObjectCommand({
-    Bucket: bucket,
+    Bucket: bucketName,
     Key: key,
   });
 
@@ -38,21 +34,5 @@ export const handler = async (event: S3Event) => {
       console.log("RESULT, ALL PARSED PRODUCTS", result);
     });
 
-  const copyObjectCommand = new CopyObjectCommand({
-    Bucket: bucket,
-    CopySource: `${bucket}/${key}`,
-    Key: key.replace("uploaded", "parsed"),
-  });
-
-  const deleteObjectCommand = new DeleteObjectCommand({
-    Bucket: bucket,
-    Key: key,
-  });
-
-  const copyResponse = await s3Client.send(copyObjectCommand);
-  const deleteResponse = await s3Client.send(deleteObjectCommand);
-
-  console.log("COPY RESPONSE", copyResponse);
-  console.log("DELETE RESPONSE", deleteResponse);
-  console.log("File moved from uploaded/ to parsed/ folder successfully");
+  await moveParsedFile(s3Client, key, bucketName);
 };
