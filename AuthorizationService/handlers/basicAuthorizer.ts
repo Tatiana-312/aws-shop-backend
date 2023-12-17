51a597f0955ec5869b66f5b078bb37fa9aea6410
Tatiana Kuznetsova <tatiana_kuznetsova@epam.com>
2023-12-17 16:18:23 +0600

    refactor basic authorizer

--- AuthorizationService/handlers/basicAuthorizer.ts
+++ AuthorizationService/handlers/basicAuthorizer.ts
@@ -1,4 +1,4 @@
-import { APIGatewayTokenAuthorizerEvent, PolicyDocument } from "aws-lambda";
+import { APIGatewayTokenAuthorizerEvent } from "aws-lambda";
 import { APIGatewayAuthorizerResult } from "aws-lambda/trigger/api-gateway-authorizer";
 import "dotenv/config";
 
@@ -7,13 +7,30 @@ export const handler = async (
 ): Promise<APIGatewayAuthorizerResult> => {
   console.log("event =>", event);
 
+  const generateResponse = (effect: string = "Deny") => ({
+    principalId: "principalId",
+    policyDocument: {
+      Version: "2012-10-17",
+      Statement: [
+        {
+          Action: "execute-api:Invoke",
+          Effect: effect,
+          Resource: event.methodArn,
+        },
+      ],
+    },
+  });
+
   try {
-    if (event.type !== "TOKEN") {
-      throw new Error("Unauthorized"); // Return a 401 Unauthorized response
-    }
     const authorizationToken = event.authorizationToken;
 
     const base64Credentials = authorizationToken.split(" ")[1];
+
+    if (!authorizationToken) {
+      console.log("No Authorization token");
+      throw Error("Unauthorized");
+    }
+
     const buffer = Buffer.from(base64Credentials, "base64");
     const [username, password] = buffer.toString("utf-8").split(":");
 
@@ -21,37 +38,11 @@ export const handler = async (
 
     const secretUserPassword = process.env[username];
     const effect =
-      !secretUserPassword || secretUserPassword !== password ? "Deny" : "Allow"; // Return a 403 response if Deny
+      !secretUserPassword || secretUserPassword !== password ? "Deny" : "Allow";
 
-    const generatePolicyDocument = (
-      effect: "Allow" | "Deny",
-      resource: string
-    ) => ({
-      Version: "2012-10-17",
-      Statement: [
-        {
-          Action: "execute-api:Invoke",
-          Effect: effect,
-          Resource: resource,
-        },
-      ],
-    });
-
-    const policyDocument: PolicyDocument = generatePolicyDocument(
-      effect,
-      event.methodArn
-    );
-
-    const response: APIGatewayAuthorizerResult = {
-      principalId: base64Credentials,
-      policyDocument,
-    };
-
-    console.log(`response => ${JSON.stringify(response)}`);
-
-    return response;
+    return generateResponse(effect);
   } catch (error) {
     console.log("Invalid auth token. error => ", error);
-    throw new Error("Unauthorized"); // Return a 401 Unauthorized response
+    return generateResponse();
   }
 };
